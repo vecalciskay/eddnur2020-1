1d657b5820f1cf8d49f8808bacfc1993ef0d223a
Vladimir Calderon <vladimir.calderon@gmail.com>
2020-06-23 07:25:10 -0400

    Cambios de martes 23

--- ClasesEstructuraDatos/src/archivo/daoafs/dao/DAOFactoryMysql.java
+++ ClasesEstructuraDatos/src/archivo/daoafs/dao/DAOFactoryMysql.java
@@ -1,5 +1,12 @@
 package archivo.daoafs.dao;
 
+/**
+ * La implementación del Dao Factory para MySql. Aquí se crea toda la familia
+ * de DAOs que usarán MySql como soporte de base de datos.
+ * 
+ * @author Vladimir
+ *
+ */
 public class DAOFactoryMysql extends DAOFactory {
 
 	@Override
